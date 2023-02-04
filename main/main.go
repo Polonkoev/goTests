010b640a4fc7665bc701063708d4b80c411047dd
polonkoev <polonkoev.temerlan@gmail.com>
2023-02-04 11:37:50 +0600

    cycle fork

--- main/main.go
+++ main/main.go
@@ -52,3 +52,19 @@
 // 	fmt.Println(j)
 
 // }
+
+package main
+
+import "fmt"
+
+func main() {
+	x := 6
+	res := 0
+	for i := 0; i < x; i++ {
+		res = i
+		if i%2 == 0 &&  {
+			fmt.Println(res)
+		}
+	}
+
+}
